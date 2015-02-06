cc219b5a10fd1bc16bf6b826a5852184861a3120
LinhTK <linh.truongthikieu@harveynash.vn>
2015-02-06 17:31:09 +0700

    update

---
+++ admin/contact/index.php
@@ -0,0 +1,44 @@
+<?php
+	
+	include "functions.php";
+	$title_page="Danh sách hỗ trợ";
+	// update status
+	$edit_id=$_REQUEST['edit_id'];
+	$bool_field=$_POST['bool_field'];
+	$edit_id=$_POST['edit_id'];
+	if($bool_field)
+	{
+		$sql="UPDATE ".TABLE_PREFIX."support"." SET ".$bool_field."= not ".$bool_field." WHERE md5(".$table_name."_id)='$edit_id'";
+		execSQL($sql); 
+	}
+	$condition=" WHERE 1=1 ";
+
+	$sql="SELECT md5(id) AS edit_id
+				,fullname
+				,address
+				,phone
+				,email
+				FROM ".TABLE_PREFIX."support";
+				
+	$rs=execSQL($sql);
+	$row_total=mysql_num_rows($rs);
+	$num=0;
+	while ($row=mysql_fetch_assoc($rs))
+	{
+		$edit_id=$row['edit_id'];
+		$row['no']=++$num;
+		$xtpl->assign('LIST',$row);
+		$xtpl->parse('main.LIST');
+	}			
+	
+	$xtpl->assign('show_result',$limit);								
+	$xtpl->assign('COM_LBL_SEARCH',COM_LBL_SEARCH);	
+	$xtpl->assign('COM_SHOW_RESULT',COM_SHOW_RESULT);								
+	$xtpl->assign('COM_LBL_NO',COM_LBL_NO);
+	$xtpl->assign('COM_LBL_ACTION',COM_LBL_ACTION);
+	$xtpl->assign('COM_LBL_CHECKALL',COM_LBL_CHECKALL);
+	$xtpl->assign('COM_LBL_TOTAL',sprintf(COM_LBL_TOTAL,$row_total));
+	
+	$xtpl->assign('MOD',$mod);	
+	$xtpl->assign('title_page',$title_page);			
+?>
---
+++ admin/share/action.php
@@ -0,0 +1,116 @@
+<?php
+	include "functions.php";
+	include "setting.php";
+	$title_page="Thêm mới - Cập nhật";
+	$action=$_POST['frmAction'];
+	$edit_id=$_REQUEST['edit_id'];
+	$error="";
+	if($action=="Action")
+	{
+		$fullname=$_POST['fullname'];
+		$phone=$_POST['phone'];
+		$email=$_POST['email'];
+		$address=$_POST['address'];
+        $content=$_POST['content'];
+		$edit_id=$_POST['edit_id'];
+		
+		if($fullname=="")
+		{
+			$error.=sprintf(ERR_NULL,"Họ tên");
+		}
+		if($phone=="")
+		{
+			$error.=sprintf(ERR_NULL,"Số điện thoại");
+		}
+		if($content=="")
+		{
+			$error.=sprintf(ERR_NULL,"Nội dung");
+		}
+
+		if($error)
+		{
+			$xtpl->assign('error',$error);
+			$xtpl->parse('main.error');
+		}
+		else
+		{
+			if($edit_id)
+			{
+				
+				$sql="UPDATE ".TABLE_PREFIX."share
+						SET 
+							fullname='$fullname'
+							,phone='$phone'
+							,email='$email'
+							,content='$content'
+							,address='$address'
+						WHERE md5(id)='$edit_id'";
+				execSQL($sql);
+				redir('index.php?mod=share');
+				exit();		
+			
+			}
+			else
+			{
+				$sql="INSERT INTO ".TABLE_PREFIX."share(
+						fullname
+						,phone
+						,email
+						,content
+						,address
+						) VALUES(
+						'$fullname'
+						,'$phone'
+						,'$email'
+						,'$content'
+						,'$address'
+						)";
+				execSQL($sql);
+				redir('index.php?mod=share');
+				exit();
+			}
+		}
+	}
+	else
+	{
+		if($edit_id){
+			$sql="SELECT fullname
+						,phone
+						,email
+						,content
+						,address
+						,md5(id) AS edit_id
+					FROM ".TABLE_PREFIX."support
+				 WHERE md5(id)='$edit_id'";
+			$row=recordset($sql);
+			$fullname					= $row['fullname'];
+			$phone 				= $row['phone'];
+			$email 				= $row['email'];
+			$content 				= $row['content'];
+            $address 				= $row['address'];
+			$edit_id 				= $row['edit_id'];
+			
+		}
+	}
+	
+	$input_fullname			= gen_input_text('fullname',$fullname,50,255,'','a_text');
+	$input_phone			= gen_input_text('phone',$phone,50,255,'','a_text');
+	$input_email			= gen_input_text('email',$email,50,255,'','a_text');
+    $input_address			= gen_input_text('address',$address,50,255,'','a_text');
+	$input_content				= gen_input_FCKEditor('content',$content);
+	$input_hidden_edit_id		= gen_input_hidden('edit_id',$edit_id);
+	
+	$xtpl->assign('input_fullname',$input_fullname);
+	$xtpl->assign('input_phone',$input_phone);
+	$xtpl->assign('input_email',$input_email);
+	$xtpl->assign('input_content',$input_content);
+    $xtpl->assign('input_address',$input_address);
+	
+	$xtpl->assign('input_hidden_edit_id',$input_hidden_edit_id);
+	$xtpl->assign('COM_ADD_UPDATE',COM_ADD_UPDATE);
+	$xtpl->assign('COM_RESET',COM_RESET);
+	$xtpl->assign('COM_BUTTON_BACK',COM_BUTTON_BACK);
+	$xtpl->assign('MOD',get('mod'));
+	$xtpl->assign('title_page',$title_page);
+	
+?>
